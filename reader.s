57922daa8f3b3c33f8d1af4c8a52de4209d64059
ketrab100 <bartis300@gmail.com>
2021-06-08 22:26:28 +0200

    lala

--- reader.s
+++ reader.s
@@ -24,36 +24,23 @@ rightCode:
 
 codeValue: .long 0
 .text                       
-.globl _start                
-_start:
-# open file
-    movl $5, %eax # sys_open
-    movl $name, %ebx
-    movl $0, %ecx # access: read-only
-    movl $0777, %edx # read, write and execute by all
-    int $0x80
-
-# get file descriptor
-    movl %eax, fd
-
-# read from file
-    movl $3, %eax # sys_read
-    movl fd, %ebx
-    movl $buf, %ecx
-    movl $len, %edx
-    int $0x80
-
-# close file
-    movl $6, %eax # sys_close
-    movl $name, %ebx
-    int $0x80
+.globl reader                
+reader:
+  push %ebp
+  mov %esp, %ebp
+  
+  mov 8(%ebp), %ebx
+  mov %ebx, buf
 
 # get size
-  movl $18, %edi 
-  movl buf(,%edi,1), %eax
+  mov buf, %edi
+  add $18, %edi 
+  mov 0(%edi), %eax
   movl %eax, sizex
-  movl $22, %edi 
-  movl buf(,%edi,1), %eax
+
+  mov buf, %edi
+  add $22, %edi 
+  mov 0(%edi), %eax
   movl %eax, sizey
 
 # calcPaddingBytes
@@ -310,8 +297,7 @@ convert1:
     je exit
     jmp loop3
 exit:
- # exit(0)
-    movl    $1, %eax  
-    movl    $0, %ebx   
-    int     $0x80     
+
+    pop %ebp
+    ret
 
