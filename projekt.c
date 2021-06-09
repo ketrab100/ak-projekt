a5870c17bfc9e005631eb9d3365c2ffe21f9c831
ketrab100 <bartis300@gmail.com>
2021-06-09 22:57:04 +0200

    a

--- projekt.c
+++ projekt.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include<fcntl.h>
+#include <fcntl.h>
 
 long reader(char*);
 int main()
 {
     int fd = 0;
-    char name [] = "/home/bartek/Desktop/ak/ak-projekt/ean85.bmp";
-
+    char name [100];
+    printf("Prosze poodac nazwe pliku\n");
+    scanf("%s",name);
     fd = open(name,O_RDWR,O_APPEND);
     char* buf = malloc(30000000);
     read(fd,buf,30000000);
@@ -16,5 +17,5 @@ int main()
     long result = 0;
     result = reader(a);
     result/=10;
-    printf("%ld", result);
+    printf("%ld\n", result);
 }
